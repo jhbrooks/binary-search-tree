e66b2deea369c7f5130d62feef60f1bfa87f5c40
James Brooks <jhbrooks@uchicago.edu>
2015-11-24 12:50:53 -0500

    refactor for clarity; improve display; remove some testing code

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,5 +1,6 @@
 #!/Users/jhbrooks/.rvm/rubies/ruby-2.2.0/bin/ruby
 
+# This class builds and searches binary search trees
 class Node
   def self.build_tree(array)
     tree = Node.new(nil, nil, nil, nil)
@@ -9,95 +10,85 @@ class Node
     tree
   end
 
-  attr_accessor :parent, :value, :l_child, :r_child
+  attr_accessor :parent, :value, :l_child, :r_child, :line_width
 
   def initialize(parent, value, l_child, r_child)
     @parent = parent
     @value = value
     @l_child = l_child
     @r_child = r_child
+    @line_width = 80
   end
 
   def place_new_value(new_value)
     if value.nil?
       self.value = new_value
     elsif new_value < value
-      self.l_child = Node.new(self, nil, nil, nil) if l_child.nil?
+      self.l_child ||= Node.new(self, nil, nil, nil)
       l_child.place_new_value(new_value)
     else
-      self.r_child = Node.new(self, nil, nil, nil) if r_child.nil?
+      self.r_child ||= Node.new(self, nil, nil, nil)
       r_child.place_new_value(new_value)
     end
   end
 
-  def breadth_first_search(t_value, searched_nodes = [], search_queue = [self])
-    searched_nodes.push(self)
+  def breadth_first_search(targ, searched = [], queue = [self])
+    # not needed, but would be if heirarchy were more complex
+    searched.push(self)
 
-    p value
-    if value == t_value
-      self
-    else
-      search_queue.shift
+    return self if value == targ
 
-      search_queue.push(l_child) unless searched_nodes.include?(l_child) || l_child.nil?
-      search_queue.push(r_child) unless searched_nodes.include?(r_child) || r_child.nil?
+    queue.shift
+    queue.push(l_child) unless searched.include?(l_child) || l_child.nil?
+    queue.push(r_child) unless searched.include?(r_child) || r_child.nil?
 
-      if !search_queue.empty?
-        next_node = search_queue.first
-        next_node.breadth_first_search(t_value, searched_nodes, search_queue)
-      else
-        nil
-      end
-    end
+    return nil if queue.empty?
+
+    next_node = queue.first
+    next_node.breadth_first_search(targ, searched, queue)
   end
 
-  def depth_first_search(t_value, searched_nodes = [], search_stack = [self])
-    searched_nodes.push(self)
+  def depth_first_search(targ, searched = [], stack = [self])
+    # not needed, but would be if heirarchy were more complex
+    searched.push(self)
 
-    p value
-    if value == t_value
-      self
-    else
-      search_stack.pop
+    return self if value == targ
 
-      search_stack.push(r_child) unless searched_nodes.include?(r_child) || r_child.nil?
-      search_stack.push(l_child) unless searched_nodes.include?(l_child) || l_child.nil?
+    stack.pop
+    stack.push(r_child) unless searched.include?(r_child) || r_child.nil?
+    stack.push(l_child) unless searched.include?(l_child) || l_child.nil?
 
-      if !search_stack.empty?
-        next_node = search_stack.last
-        next_node.depth_first_search(t_value, searched_nodes, search_stack)
-      else
-        nil
-      end
-    end
+    return nil if stack.empty?
+
+    next_node = stack.last
+    next_node.depth_first_search(targ, searched, stack)
   end
 
-  def dfs_rec(t_value)
-    p value
-    if value == t_value
+  def dfs_rec(targ)
+    # no search tracking, so could fail if heirarchy were more complex
+    if value == targ
       self
     else
-      (l_child && l_child.dfs_rec(t_value)) || (r_child && r_child.dfs_rec(t_value))
+      (l_child && l_child.dfs_rec(targ)) || (r_child && r_child.dfs_rec(targ))
     end
   end
 
   def to_s
-    # provisional
-    # should be easier to improve after defining search methods
-    "V: #{value} L[ #{l_child} ] R[ #{r_child} ]"
+    return_array = ["P: #{parent && parent.value}",
+                    "|",
+                    "V: #{value}",
+                    "/ \\",
+                    "L: #{l_child && l_child.value} "\
+                    "R: #{r_child && r_child.value}"]
+    return_array.map { |e| e.center(line_width) }.join("\n")
   end
 end
 
 tree = Node.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p tree.breadth_first_search(9).object_id
-puts "---"
-p tree.breadth_first_search(6).object_id
-puts "---"
-p tree.depth_first_search(9).object_id
-puts "---"
-p tree.depth_first_search(6).object_id
-puts "---"
-p tree.dfs_rec(9).object_id
-puts "---"
-p tree.dfs_rec(6).object_id
-puts "---"
+puts "Root node of tree:\n#{tree}"
+puts "BFS for value in tree (9):\n#{tree.breadth_first_search(9)}"
+puts "BFS for value not in tree (6):\n#{tree.breadth_first_search(6)}"
+puts "DFS for value in tree (9):\n#{tree.depth_first_search(9)}"
+puts "DFS for value not in tree (6):\n#{tree.depth_first_search(6)}"
+puts "Recursive DFS for value in tree (9):\n#{tree.dfs_rec(9)}"
+puts "Recursive DFS for value not in tree (6):\n#{tree.dfs_rec(6)}"
